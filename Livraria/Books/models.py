edec3493077bfb2724a8033e8dd61ef94b7ba4f0
Thamires Schifini <thamiresschifinif@gmail.com>
2020-06-07 14:53:45 -0300

    Adicionar a taxa de multa do livro por usuário

--- Livraria/Books/models.py
+++ Livraria/Books/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+TAX_LT_3_DAYS = 3
+TAX_GT_3_DAYS = 5
+TAX_GT_5_DAYS = 7
+
+
 class book(models.Model):
     AVAILABLE = 'A'
     BORROWED = 'B'
@@ -12,4 +17,5 @@ class book(models.Model):
     status = models.CharField(max_length=1, choices=status_choices, default=AVAILABLE)
     name = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
-    user = models.ForeignKey('Accounts.User', on_delete=models.CASCADE)
+    user = models.ForeignKey('Accounts.User', on_delete=models.CASCADE, null=True)
+    dia_locacao = models.DateField(null=True)
--- Livraria/Books/views.py
+++ Livraria/Books/views.py
@@ -1,8 +1,9 @@
-from Livraria.Books.models import book
+from datetime import datetime, timedelta
+from Livraria.Books.models import book, TAX_LT_3_DAYS, TAX_GT_3_DAYS, TAX_GT_5_DAYS
 from Livraria.Accounts.models import User
 from Livraria.Books.serializers import BookSerializer
 from rest_framework import viewsets
-from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
+from django.http import HttpResponseNotFound, JsonResponse
 
 
 # Create your views here.
@@ -17,25 +18,53 @@ def book_by_user_id(request, id_client=0):
     if not User.objects.filter(id=id_client).exists():
         return HttpResponseNotFound()
     user_instance = User.objects.get(id=id_client)
-    serializer = BookSerializer(book.objects.filter(user=user_instance), many=True)
-    return JsonResponse(serializer.data, safe=False)
+
+    book_list = []
+    for b in book.objects.filter(user=user_instance):
+        book_dict = BookSerializer(b).data
+        book_dict['tax [%]'] = tax_by_book(b.dia_locacao)
+        book_dict['day_location'] = b.dia_locacao
+
+        book_list.append(book_dict)
+
+    return JsonResponse(book_list, safe=False)
 
 def reserve_by_id_client(request, id_book=0, id_client=0):
     if id_book < 0:
         return HttpResponseNotFound()
     if not book.objects.filter(id=id_book).exists():
-        return HttpResponseNotFound()
+        return HttpResponseNotFound('livro n existe')
     book_instance = book.objects.get(id=id_book)
     if not book_instance.status == book.AVAILABLE:
-        return HttpResponseNotFound('sdf')
+        return HttpResponseNotFound('livro n disponivel')
     if not User.objects.filter(id=id_client).exists():
-        return HttpResponseNotFound()
+        return HttpResponseNotFound('usuario n existe')
     user_instance = User.objects.get(id=id_client)
     book_instance.user = user_instance
     book_instance.status = book.BORROWED
+    book_instance.dia_locacao = datetime.now().date()
     book_instance.save()
     serializer = BookSerializer(book_instance)
     return JsonResponse(serializer.data)
+
+def tax_by_book(data_locacao_book):
+    date_now = datetime.now().date()
+    date_atraso = date_now - data_locacao_book
+    delta_days = date_atraso.days - 3
+
+    if delta_days <= 0:
+        total_tax = 0
+
+    elif delta_days <= 3:
+        total_tax = (TAX_LT_3_DAYS+(0.2*delta_days))
+
+    elif delta_days <= 5:
+        total_tax = (TAX_GT_3_DAYS+(0.4*delta_days))
+
+    elif delta_days > 5:
+        total_tax =(TAX_GT_5_DAYS+(0.6*delta_days))
+
+    return total_tax
 
 
 #def books(request):
