443e942d006367723010c3d663230c03e8feb889
Thamires Schifini <thamiresschifinif@gmail.com>
2020-06-07 22:40:58 -0300

    Permitir conexao de qualquer host

--- Livraria/settings.py
+++ Livraria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e(j$o46o8hux==bz@ei0nz^bu7*%vf%vh#*)+9-5(5n5l=*_g^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
